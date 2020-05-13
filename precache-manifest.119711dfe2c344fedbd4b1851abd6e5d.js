5c06a2916523e9ec76cec65237c593c81a4e45b4
orangefoil <orangefoil@users.noreply.github.com>
2020-05-13 17:18:57 +0000

    deploy: 7b5f6754aa47b15d9a87e08c147425dad74c3178

---
+++ precache-manifest.119711dfe2c344fedbd4b1851abd6e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48f3b365f19ed7f8fdb8131fea6c5c3",
+    "url": "/ss20-webtools-projekt/index.html"
+  },
+  {
+    "revision": "7477ddc2107d699825ae",
+    "url": "/ss20-webtools-projekt/static/css/main.2810f382.chunk.css"
+  },
+  {
+    "revision": "7aa443aff4c8196d6119",
+    "url": "/ss20-webtools-projekt/static/js/2.9181b189.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/ss20-webtools-projekt/static/js/2.9181b189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7477ddc2107d699825ae",
+    "url": "/ss20-webtools-projekt/static/js/main.456ad818.chunk.js"
+  },
+  {
+    "revision": "156ef26e8475cdfad8c1",
+    "url": "/ss20-webtools-projekt/static/js/runtime-main.00bcc520.js"
+  }
+]);
