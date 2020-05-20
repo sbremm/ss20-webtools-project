b2f29115fb3605b4dbfc59c7c8b89e0fe2b4497a
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-20 17:54:25 +0200

    Sort data in data table

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -9,7 +9,7 @@ const DataTable = ({ data }) => {
           <th>X</th>
           <th>Y</th>
         </tr>
-        {data.map(value =>
+        {data.sort().map(value =>
           <tr>
             <td>{Number.parseFloat(value[0]).toFixed(2)}</td>
             <td>{Number.parseFloat(value[1]).toFixed(2)}</td>
