7adb3ccd1a2304ff90a64dec506225da7769970f
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-03 14:25:00 +0200

    Simplified Material UI imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
+import {
+  makeStyles,
+  Grid,
+  Paper,
+  Typography,
+} from '@material-ui/core'
 import PCA from 'pca-js'
 import AdjustedData from './components/AdjustedData'
 import DataTable from './components/DataTable'
