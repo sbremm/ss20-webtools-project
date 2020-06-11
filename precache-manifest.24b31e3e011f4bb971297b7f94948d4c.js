ea6d48faab0a0ac5a2195c261dfd4c6c0006cf1e
orangefoil <orangefoil@users.noreply.github.com>
2020-06-11 10:31:22 +0000

    deploy: 825d632f31717d3499fdcd2ddc3ffa26ca65a4a4

---
+++ precache-manifest.24b31e3e011f4bb971297b7f94948d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fae8bee710eb36ad1f2733410231bb5",
+    "url": "/ss20-webtools-project/index.html"
+  },
+  {
+    "revision": "5c0240811d1b0a830ffd",
+    "url": "/ss20-webtools-project/static/css/main.fd75bbe3.chunk.css"
+  },
+  {
+    "revision": "715d2ede7170841779d2",
+    "url": "/ss20-webtools-project/static/js/2.fdffe90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ss20-webtools-project/static/js/2.fdffe90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0240811d1b0a830ffd",
+    "url": "/ss20-webtools-project/static/js/main.3617fe34.chunk.js"
+  },
+  {
+    "revision": "abe76b22a795ca005d04",
+    "url": "/ss20-webtools-project/static/js/runtime-main.f552bc4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ss20-webtools-project/precache-manifest.442de5ecc5253e8b0b7e4b165f902cf1.js"
+  "/ss20-webtools-project/precache-manifest.24b31e3e011f4bb971297b7f94948d4c.js"
 );
 
 self.addEventListener('message', (event) => {
