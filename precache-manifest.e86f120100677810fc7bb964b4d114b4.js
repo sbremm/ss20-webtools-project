823c542b2d36f76be152bc898ed448008f431cb5
orangefoil <orangefoil@users.noreply.github.com>
2020-06-17 14:27:20 +0000

    deploy: cd9f832de5cd8f1f8a371e7c39a78c4af406e65b

--- precache-manifest.e86f120100677810fc7bb964b4d114b4.js
+++ precache-manifest.e86f120100677810fc7bb964b4d114b4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ae7230c4eb06fa9ed2e3e28fe63d42b",
+    "revision": "fc9057676f6276153f64638ef90e7812",
     "url": "/ss20-webtools-project/index.html"
   },
   {
-    "revision": "4f1fabd38aaf73407889",
+    "revision": "1383e91f98988250b6c1",
     "url": "/ss20-webtools-project/static/css/main.1f3dfa8d.chunk.css"
   },
   {
-    "revision": "6fdbce98befe00db2480",
-    "url": "/ss20-webtools-project/static/js/2.9d1f35b9.chunk.js"
+    "revision": "1d55c15f61aa703b1b9c",
+    "url": "/ss20-webtools-project/static/js/2.a9174d75.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/ss20-webtools-project/static/js/2.9d1f35b9.chunk.js.LICENSE.txt"
+    "url": "/ss20-webtools-project/static/js/2.a9174d75.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f1fabd38aaf73407889",
-    "url": "/ss20-webtools-project/static/js/main.5610463a.chunk.js"
+    "revision": "1383e91f98988250b6c1",
+    "url": "/ss20-webtools-project/static/js/main.75d42f4a.chunk.js"
   },
   {
     "revision": "9fbb728adc08e1b557e4",
