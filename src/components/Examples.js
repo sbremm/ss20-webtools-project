da16ec5907ea20fff3f9e9cca0317df5aa9025d8
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-18 16:57:05 +0200

    Tooltips for examples

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 const examples = [
   {
     'title': 'One',
+    'tooltip': 'This is the first example',
     'data': [[1,2], [2,1], [-1,-2], [-4,-3]]
   },
   {
     'title': 'Two',
+    'tooltip': 'This is the second example',
     'data': [[9,-7], [12,3], [-8,-8]]
   }
 ]
@@ -20,12 +22,18 @@ const Examples = ({ setData }) => {
       </Card.Header>
       <Card.Body>
         {examples.map((example, index) =>
-          <Button
-            key={index}
-            variant="secondary"
-            block
-            onClick={() => setData(example.data)}>{example.title}
-          </Button>
+          <OverlayTrigger key={index} placement="right" overlay={
+            <Tooltip id={'tooltip-right'}>
+              {example.tooltip}
+            </Tooltip>
+          }>
+            <Button
+              variant="secondary"
+              block
+              onClick={() => setData(example.data)}>{example.title}
+            </Button>
+          </OverlayTrigger>
+
         )}
       </Card.Body>
     </Card>
