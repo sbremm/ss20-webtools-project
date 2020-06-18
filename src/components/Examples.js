ea1926c74d2b5455d7d92d043573e2057bb1bb4b
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-18 17:17:19 +0200

    Extracted hardcoded examples into separate data file

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,20 +1,6 @@
 import React, { useState } from 'react'
 import { Button, Card, OverlayTrigger, Tooltip } from 'react-bootstrap'
-
-const examples = [
-  {
-    'title': 'One',
-    'tooltip': 'This is the first example',
-    'description': 'This is not particularly interesting.',
-    'data': [[1,2], [2,1], [-1,-2], [-4,-3]]
-  },
-  {
-    'title': 'Two',
-    'tooltip': 'This is the second example',
-    'description': 'Lorem ipsum dolor sit amet.',
-    'data': [[9,-7], [12,3], [-8,-8]]
-  }
-]
+import examples from '../data/examples'
 
 const Examples = ({ setData }) => {
   const [activeExample, setActiveExample] = useState(null)
---
+++ src/data/examples.js
@@ -0,0 +1,16 @@
+const examples = [
+  {
+    'title': 'One',
+    'tooltip': 'This is the first example',
+    'description': 'This is not particularly interesting.',
+    'data': [[1,2], [2,1], [-1,-2], [-4,-3]]
+  },
+  {
+    'title': 'Two',
+    'tooltip': 'This is the second example',
+    'description': 'Lorem ipsum dolor sit amet.',
+    'data': [[9,-7], [12,3], [-8,-8]]
+  }
+]
+
+export default examples
