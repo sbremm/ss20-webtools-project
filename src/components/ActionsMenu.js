c3ea294d42c3faf2d18e8c3a60e17f30e0a17cef
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-20 00:09:57 +0200

    Data import and export

--- src/components/ActionsMenu.js
+++ src/components/ActionsMenu.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Alert, Button, Card, Form } from 'react-bootstrap'
 import PCA from 'pca-js'
 
 const ActionsMenu = ({ data, setData, setHighlightedComponent, setHighlightedIndex }) => {
+  const [uploadErrorMessage, setUploadErrorMessage] = useState(null)
+
   const generateRandomScatterPlot = () => {
     let newData = []
     for (let i = 0; i < 20; i++) {
@@ -12,6 +14,23 @@ const ActionsMenu = ({ data, setData, setHighlightedComponent, setHighlightedInd
       ]])
     }
     setData(newData)
+  }
+
+  const handleUpload = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    if (!file) return
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      try {
+        const newData = JSON.parse(event.target.result)
+        setData(newData)
+      } catch (exception) {
+        setUploadErrorMessage('Unable to parse as JSON file')
+        setTimeout(() => setUploadErrorMessage(null), 5000)
+      }
+    }
+    reader.readAsText(file)
   }
 
   const reset = () => {
@@ -32,6 +51,29 @@ const ActionsMenu = ({ data, setData, setHighlightedComponent, setHighlightedInd
         {data.length > 0 ?
           <Button variant="secondary" block onClick={() => setData(PCA.computeDeviationMatrix(data))}>Center Data</Button> :
           <Button variant="secondary" block disabled>Center Data</Button>
+        }
+        <Button
+          variant="secondary"
+          block
+          href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`}
+          download="data.json"
+        >
+          Export data
+        </Button>
+        <Form>
+          <Form.File
+            label="Import JSON file"
+            custom
+            onChange={handleUpload}
+          />
+        </Form>
+        {
+          uploadErrorMessage ?
+            <Alert variant="danger">
+              {uploadErrorMessage}
+            </Alert>
+            :
+            ''
         }
       </Card.Body>
     </Card>
