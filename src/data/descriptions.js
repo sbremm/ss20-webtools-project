0d5aea7c6f1978e5e9c9fb017cd14c228f3918b2
Marcus Legendre <marcus.legendre@gmail.com>
2020-07-02 17:35:52 +0200

    Updated descriptions

--- src/data/descriptions.js
+++ src/data/descriptions.js
@@ -3,28 +3,46 @@ import { Image } from 'react-bootstrap'
 
 const About = {
   'title': 'About',
-  'text': 'This text explains the purpose of this website.'
+  'text': (
+    <>
+      This website will hopefully help you to get a better understanding of the
+      <a href="https://en.wikipedia.org/wiki/Principal_component_analysis">principal component analysis</a> (PCA).<br />
+      Pick one of the interactive examples or enter your own data,
+      and see how this affects the PCA that is automatically calculated in the background.
+    </>
+  )
 }
 
 const ComponentN = {
   'title': 'Component',
   'shortText': 'This is what the data looks like after reducing from two to one dimension',
-  'longText': 'Lorem ipsum dolor sit amet. AdjustedData longText. TODO'
+  'longText': (
+    <>
+      This component graph shows the data adjusted for the nth principal component. It is the result of projecting the
+      data points onto the principal component vector that you can see in the scatter plot.<br />
+      When you hover your mouse over the component graph you will see red lines in the scatter plot. These lines are
+      called residues. If the data points in the scatter plot would be moved along the residues onto the component
+      vector, you get the exact component graph shown here.
+    </>
+  )
 }
 
 const EigenvaluesChart = {
   'title': 'Eigenvalues',
   'shortText': 'The eigenvalues indicate the significance of the components relative to each other.',
-  'longText': 'Lorem ipsum dolor sit amet. EigenvaluesChart longText. TODO'
+  'longText': (
+    <>
+      Principal Components are always sorted in order of significance. So the first component will always be
+      the one that explains the most data.<br />
+      The absolute value of the eigenvalues is not that important. High abolute eigenvalues only indicate that the
+      data points are comprised of big numbers.
+    </>
+  )
 }
 
 const ScatterPlot = {
   'title': 'Scatter plot with component vectors',
-  'shortText': (
-    <>
-      Example using <u>HTML</u>. Although technically this is RSX.
-    </>
-  ),
+  'shortText': 'The two colored lines are the principal components vectors.',
   'longText': (
     <>
       <p>
