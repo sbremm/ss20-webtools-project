ef8fae93b2e358493afe0980661846cca6d84bfd
Marcus Legendre <marcus.legendre@gmail.com>
2020-07-09 12:54:47 +0200

    Examples: replaced Card component with Modal

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,5 +1,5 @@
-import React  from 'react'
-import { Button, Card, Modal, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import React from 'react'
+import { Button, Modal, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import examples from '../data/examples'
 
 const Examples = ({ setActiveExample, setExampleStep, setData, showExamplesModal, setShowExamplesModal }) => {
@@ -11,32 +11,32 @@ const Examples = ({ setActiveExample, setExampleStep, setData, showExamplesModal
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Card>
-        <Card.Header as="h3">
-        Examples
-        </Card.Header>
-        <Card.Body>
-          {examples.map((example, index) =>
-            <OverlayTrigger key={index} placement="right" overlay={
-              <Tooltip id={'tooltip-right'}>
-                {example.tooltip}
-              </Tooltip>
-            }>
-              <Button
-                variant="secondary"
-                block
-                onClick={() => {
-                  setActiveExample(example)
-                  setExampleStep(0)
-                  setData(example.steps[0].data)
-                }}
-              >
-                {example.title}
-              </Button>
-            </OverlayTrigger>
-          )}
-        </Card.Body>
-      </Card>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          Examples
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {examples.map((example, index) =>
+          <OverlayTrigger key={index} placement="right" overlay={
+            <Tooltip id={'tooltip-right'}>
+              {example.tooltip}
+            </Tooltip>
+          }>
+            <Button
+              variant="secondary"
+              block
+              onClick={() => {
+                setActiveExample(example)
+                setExampleStep(0)
+                setData(example.steps[0].data)
+              }}
+            >
+              {example.title}
+            </Button>
+          </OverlayTrigger>
+        )}
+      </Modal.Body>
     </Modal>
   )
 }
